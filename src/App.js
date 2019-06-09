d1e8f23444981435734be267b4efa527b6d355b4
Kaai Suzuki <suzydp@me.com>
2019-06-09 12:21:52 -0700

    Prepared for routing to add multiple animations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory'
 import RotatingBox from './RotatingBox';
 import logo from './logo.svg';
 import './App.css';
 
+const appHistory = createBrowserHistory();
+
 function App() {
   return (
     <div className="App">
-      <RotatingBox />
+      <Router history={appHistory}>
+        <p>Let's start WebGL with Three.js</p>
+        <RotatingBox />
+      </Router>
     </div>
   );
 }
