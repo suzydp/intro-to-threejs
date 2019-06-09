133a90357e775426041926290298352c2b652533
Kaai Suzuki <suzydp@me.com>
2019-06-09 11:41:26 -0700

    Modified from let to this with fixing indent

--- src/RotatingBox.js
+++ src/RotatingBox.js
@@ -1,42 +1,63 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 class RotatingBox extends Component {
- componentDidMount() {
-   const width = this.rotatingBox.clientWidth;
-   const height = this.rotatingBox.clientHeight;
+  componentDidMount() {
+    const width = this.rotatingBox.clientWidth;
+    const height = this.rotatingBox.clientHeight;
 
-   let scene = new THREE.Scene();
-   let camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-   let renderer = new THREE.WebGLRenderer();
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    this.renderer = new THREE.WebGLRenderer();
+    this.controls = new OrbitControls(this.camera, this.render.domElement);
 
-   renderer.setSize(width, height);
-   this.rotatingBox.appendChild(renderer.domElement);
+    this.renderer.setSize(width, height);
+    this.rotatingBox.appendChild(this.renderer.domElement);
 
-   var geometry = new THREE.BoxGeometry(1, 1, 1);
-   var material = new THREE.MeshBasicMaterial({ color: '#bada55' });
-   var cube = new THREE.Mesh(geometry, material);
+    var texture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/crate.gif');
+    var material = new THREE.MeshBasicMaterial({ map:texture })
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    // var material = new THREE.MeshBasicMaterial({ color: '#bada55' });
 
-   scene.add(cube);
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+    this.animate(this);
+  }
 
-   camera.position.x = 0;
-   camera.position.y = 0;
-   camera.position.z = 5;
+   componentWillUnmount() {
+    cancelAnimationFrame(this.frameId);
+    this.mount.removeChild(this.renderer.domElement);
+  }
 
-   cube.position.x = 0;
-   cube.position.y = 0;
-   cube.position.z = 0;
+  initializeOrbits = () => {
+    this.controls.rotateSpeed = 1.0;
+    this.controls.zoomSpeed = 1.2;
+    this.controls.panSpeed = 0.8;
+  }
+  
+  initializeCamera = () => {
+    this.camera.position.x = 0;
+    this.camera.position.y = 0;
+    this.camera.position.z = 5;
+  }
 
-   renderer.render(scene, camera);
- }
- render() {
-   return (
-     <div
-       style={{ width: '100vw', height: '100vh' }}
-       ref={rotatingBox => (this.rotatingBox = rotatingBox)}
-     />
-   );
- }
+   animate = () => {
+     // calling a request the next time cpu is free
+     this.cubeAnimation = requestAnimationFrame(this.animate);
+     this.renderer.render(this.scene, this.camera);
+     this.cube.rotation.x += 0.01;
+     this.cube.rotation.y += 0.01;
+   }
+
+  render() {
+    return (
+      <div
+        style={{ width: '100vw', height: '100vh' }}
+        ref={rotatingBox => (this.rotatingBox = rotatingBox)}
+      />
+    );
+  }
 }
 
 export default RotatingBox;
